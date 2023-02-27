25f6c1ca32c112bf9b311c6eadd91ed8b30b813e
Cristianscamilo <cristianscamilo9@gmail.com>
2023-02-26 21:41:28 -0500

    Se agrega componente "crear" en la carpeta servicio

--- src/app/modules/servicio/servicio.module.ts
+++ src/app/modules/servicio/servicio.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServicioRoutingModule } from './servicio-routing.module';
+import { CrearComponent } from './pages/crear/crear.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrearComponent
+  ],
   imports: [
     CommonModule,
     ServicioRoutingModule
