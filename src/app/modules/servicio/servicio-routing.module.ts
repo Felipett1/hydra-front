1f0184458e004202141005fafc249d6483e0ea34
Cristianscamilo <cristianscamilo9@gmail.com>
2023-02-26 23:38:48 -0500

    Se genera la sección de consulta para crear servicio

--- src/app/modules/servicio/servicio-routing.module.ts
+++ src/app/modules/servicio/servicio-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultaComponent } from './pages/consulta/consulta.component';
+import { CrearComponent } from './pages/crear/crear.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {
+    path: "", //http:localhost:4200/inicio/servicio
+    component:ConsultaComponent
+  },
+  {
+    path: "crear", //http:localhost:4200/inicio/servicio/crear
+    component: CrearComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/servicio/servicio.module.ts
+++ src/app/modules/servicio/servicio.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ServicioRoutingModule } from './servicio-routing.module';
 import { CrearComponent } from './pages/crear/crear.component';
+import { ConsultaComponent } from './pages/consulta/consulta.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
-    CrearComponent
+    CrearComponent,
+    ConsultaComponent
   ],
   imports: [
     CommonModule,
-    ServicioRoutingModule
+    ServicioRoutingModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatInputModule,
+    
+
   ]
 })
 export class ServicioModule { }
